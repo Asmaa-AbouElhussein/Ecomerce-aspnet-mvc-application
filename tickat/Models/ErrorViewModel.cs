e67943f42752fb86e0bb489b4c4a1a8a8e3bf41b
Asmaa-AbouElhussein <asa487139@gmail.com>
2022-09-17 15:15:39 +0200

    Add project files.

---
+++ tickat/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace tickat.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
